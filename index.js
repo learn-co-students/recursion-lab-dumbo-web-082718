77530cc35a9f52c9b26f22a4c1bc903854f7cf4c
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-01-08 11:34:33 -0500

    reverseString complete

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 function printString(string) {
   console.log(string[0])
-
   if (string.length > 1) {
     let subString = string.substring(1, string.length)
     printString(subString)
   } else {
     return true
   }
+}
+
+function reverseString(string) {
+  if (string === "")
+    return "";
+  else
+    return reverseString(string.substr(1)) + string.charAt(0);
+}
+
+function isPalindrome(string) {
+
+}
+
+function addUpTo() {
+
+}
+
+function maxOf() {
+
+}
+
+function includesNumber() {
+
 }
